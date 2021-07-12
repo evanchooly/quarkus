128b3f3213e8a465052e84d2421e1266b2679bdf
Justin Lee <evanchooly@redhat.com>
2021-07-12 16:48:41 -0400

    delete old model.  i don't want it after all.

--- integration-tests/kotlin-serialization/src/test/kotlin/io/quarkus/it/kotser/ModelWithJsonDeserializeUsingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus.it.kotser;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class ModelWithJsonDeserializeUsingResourceIT extends ModelWithJsonDeserializeUsingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- integration-tests/kotlin-serialization/src/test/kotlin/io/quarkus/it/kotser/ModelWithJsonNamingStrategyResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus.it.kotser;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class ModelWithJsonNamingStrategyResourceIT extends ModelWithJsonNamingStrategyResourceTest {
-
-    // Execute the same tests but in native mode.
-}
